75b511b570291d8fa7e76e12a1e34ac713f8e02f
Akash <67492990+Akash-Manikandan@users.noreply.github.com>
2023-03-19 14:02:33 +0530

    Enjoyy!!!!

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Neo4jModule } from './neo4j/neo4j.module';
 import { UserModule } from './user/user.module';
+import { ProblemModule } from './problem/problem.module';
 
 @Module({
-  imports: [Neo4jModule, UserModule, ConfigModule.forRoot()],
+  imports: [Neo4jModule, UserModule, ConfigModule.forRoot(), ProblemModule],
   controllers: [AppController],
   providers: [AppService],
 })
