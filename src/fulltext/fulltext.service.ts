679186a59b7ec2fd75c9378d0327db195aa79dd8
Akash <67492990+Akash-Manikandan@users.noreply.github.com>
2023-03-26 22:54:29 +0530

    OKKDKD

--- src/fulltext/fulltext.service.ts
+++ src/fulltext/fulltext.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Neo4jError } from 'neo4j-driver';
 import { Neo4jService } from 'src/neo4j/neo4j.service';
 import { FullTextDto } from './dto/full-text.dto';
 
@@ -9,17 +10,24 @@ export class FulltextService {
     const session = this.neo4jService.driver.session({ database: 'neo4j' });
     const query = `CALL db.index.fulltext.queryNodes("fullTextSearch", $text) YIELD node, score
                    WITH properties(node) as n,labels(node) as label,score as score
-                   RETURN {label:label[0],username:n.name,userdesc:n.desc,userImage:n.profileImg,
+                   RETURN {label:label,username:n.name,userdesc:n.desc,userImage:n.profileImg,
                    technology:n.technology,question:n.question,questiondesc:n.description,questionImage:n.problemImg, score:score}`;
-
-    const result = await session.run(query, {
-      text: fullTextDto.text,
-    });
-    const res = result.records.map((item) => {
-      return item.map((i) => {
-        return i;
-      })[0];
-    });
-    return res;
+    try {
+      const result = await session.run(query, {
+        text: fullTextDto.text,
+      });
+      const res = result.records.map((item) => {
+        return item.map((i) => {
+          return i;
+        })[0];
+      });
+      return res;
+    } catch (e) {
+      if (e instanceof Neo4jError) {
+        throw new HttpException(e.message, HttpStatus.FORBIDDEN);
+      } else {
+        throw e;
+      }
+    }
   }
 }
